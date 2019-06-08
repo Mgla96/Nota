e4a96fcd17a4de7beb89b5fae849d4c4ffc43adf
Matt Gottlieb <Matt@169-231-223-110.wireless.ucsb.edu>
2019-06-08 13:24:08 -0700

    progress on register and login on firebase

--- js/database.js
+++ js/database.js
@@ -13,6 +13,51 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   var db = firebase.firestore();
   
+
+  //User SignUp
+  function authRegister(event) {
+    event.preventDefault();
+    var registerForm = $("form[name='registerForm']");
+    var reg_email = registerForm.find('#register_email').val();
+    var reg_password = registerForm.find('#register_password').val();
+  
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(reg_email, reg_password)
+      .then(function () {
+        this.firebaseToken.innerHTML = "Registered successfully !";
+      })
+      .catch(function(err) {
+        alert(err.message);
+      })
+  }
+  
+  // User SignIn
+  function authLogin(event) {
+    event.preventDefault();
+    var loginForm = $("form[name='loginForm']");
+    var log_email = loginForm.find('#login_email').val();
+    var log_password = loginForm.find('#login_password').val();
+  
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(log_email, log_password)
+      .then(function () {
+        this.firebaseToken.innerHTML = "Sign-in Successful !";
+        console.log('sign in successful !');
+        // outputFirebaseData();
+      })
+      .catch(function(err) {
+        alert(err.message);
+      });
+  }
+  
+  function outputFirebaseData() {
+    this.firebaseToken.innerHTML = "Hello world";
+  }
+
+
+
   
 
 
